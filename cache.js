d7e8810efa0bf7b4fee125024780b1aa10e635d6
Dmitry Baranovskiy <Dmitry@Baranovskiy.com>
2010-11-30 12:51:03 +1100

    Make callback work for expired items, added prefix property and fullproof expiry. Moved to typeof for function detection due to low chance of script dealing with functions that can’t be detected this way.

--- cache.js
+++ cache.js
@@ -1,38 +1,40 @@
 window.Cache = {
 	// Global cache expiry
 	expiry: 1e4, // 10,000ms
-	
+
+	prefix: 'cache.js -> ',
+
 	// Set an item to cache
 	set: function (key, value, expiry) {
-		expiry = (expiry) ? expiry : this.expiry;
+		expiry = +expiry || +this.expiry || 1e4;
 		
 		var cache_object = JSON.stringify({
-			expiry: (+new Date + expiry),
+			expiry: +new Date + expiry,
 			data: value
 		});
-		
-		localStorage.setItem('cache.js' + key, cache_object);
-		
+
+		localStorage.setItem(this.prefix + key, cache_object);
+
 		return value;
 	},
-	
-	// Get an item from cache
-	get: function (key) {
-		var cache = localStorage.getItem('cache.js' + key);
 
-		if(cache !== null) {
+	// Get an item from cache
+	get: function (key, nullCallback) {
+		key = this.prefix + key;
+		var cache = localStorage.getItem(key);
+
+		if (cache) {
 			var object = JSON.parse(cache);
-			
-			if(object.expiry > new Date) {
+
+			if (object.expiry > new Date) {
 				return object.data;
 			}	else {
-				localStorage.removeItem('cache.js' + key);
-				return null;
+				localStorage.removeItem(key);
 			}
-		} else if (Object.prototype.toString.call(arguments[1]) == '[object Function]') {
-			return arguments[1](key);
-		} else {
-			return null;
 		}
+		if (typeof nullCallback == 'function') {
+			return nullCallback(key);
+		}
+		return null;
 	}
 };
